6a4bccbb20649eb96c87cb2c1adb710dfa0a87cc
tianxu <xueqingxu0302@gmail.com>
2022-06-18 21:59:11 -0600

    RorotatArray

--- noteBook.js
+++ noteBook.js
@@ -24,3 +24,22 @@ function removeDuplicates(nums) {
     }
     return index
 };
+
+function rotateArray(nums, k){
+    k %= nums.length
+
+    let reverse = function(i,j){
+        while(i < j){
+            let temp = nums[i]
+            nums[i] = nums[j]
+            nums[j] = temp
+            i++
+            j--
+        }
+    }
+
+    reverse(0, nums.length-1)
+    reverse(0,k-1)
+    reverse(k, nums.length-1)
+    return nums
+}
